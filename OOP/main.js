2db9ee4de8c041fc37d15f5dbdf3f74ff11e461f
M. Abduh Husaini Batubara <muhammadabdu089@gmail.com>
2022-07-08 10:06:34 +0700

    with prototype

--- OOP/main.js
+++ OOP/main.js
@@ -1,17 +1,13 @@
 // class 
-class Mail {
-    constructor() {
-        this.from = 'pengirim@dicoding.com';
-        this.contacts = []
-        this.yourOtherProperty = 'the values';
-    }
-
-    sendMessage(msg, to) {
-        console.log(`you send: ${msg} to ${to} from ${this.from}`);
-        this.contacts.push(to);
-    }
+function Mail() {
+    this.from = 'pengirim@dicoding.com';
+    this.contacts = []
+    this.yourOtherProperty = 'the values';
 }
 
-const mail1 = new Mail();
+Mail.prototype.sendMessage = function(msg, to) {
+    console.log(`you send: ${msg} to ${to} from ${this.from}`);
+    this.contacts.push(to); // menyimpan kontak baru
+}
 mail1.yourOtherProperty();
 mail1.sendMessage('hello', 'penerima@dicoding.com');
