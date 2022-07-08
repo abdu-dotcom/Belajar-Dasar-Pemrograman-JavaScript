24045125a22fdcd836a3ff96e9181f837bcea422
M. Abduh Husaini Batubara <muhammadabdu089@gmail.com>
2022-07-08 17:44:56 +0700

    myage

--- OOP/built-in-class.js
+++ OOP/built-in-class.js
@@ -1,9 +1,12 @@
-// #1 tanpa parameter, yang berarti `myDate` akan berisi tanggal dan waktu saat ini
-const myDate = new Date();
+// parameter birthday dapat berupa miliseconds ataupun date string
+const myAge = birthday => {
+    const birtday = new Date(birthday);
+    const today = Date.now(); // today menghasil nilai miliseconds saat ini
 
-const year = myDate.getFullYear()
-const month = myDate.getMonth()
-const date = myDate.getDate()
-const hour = myDate.getHours()
-const minutes = myDate.getMinutes()
-console.log(`${hour}:${minutes}`, date, month, year);
+    const diff_ms = today - birtday.getTime(); // menghitung selish nilai miliseconds hari ini dan tanggal lahir 
+    const diffDate = new Date(diff_ms);
+
+    return diffDate.getFullYear() - 1970; // 1970 adalah representasi 0 dari miliseconds
+
+}
+console.log(myAge('2001-05-12'));
