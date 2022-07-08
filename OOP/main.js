357bd95c771c5a355c8a39f26e5e95a993710d2b
M. Abduh Husaini Batubara <muhammadabdu089@gmail.com>
2022-07-08 14:52:24 +0700

    Inheritance

--- OOP/main.js
+++ OOP/main.js
@@ -1,13 +1,31 @@
-// class 
 class Mail {
-    constructor() {
-        this.from = 'pengirim@dicoding.com';
+    constructor(author) {
+        this.from = author;
+        this._contacts = [];
     }
-
     sendMessage(msg, to) {
         console.log(`you send: ${msg} to ${to} from ${this.from}`);
+        this._contacts.push(to);
+    }
+    showAllContacts() {
+        return this._contacts;
     }
 }
 
-const mail1 = new Mail();
-mail1.sendMessage('hello', 'penerima@dicoding.co');
+
+class WhatsApp extends Mail {
+    // attribut
+    constructor(author) {
+        super(author);
+        this.username = 'dicoding';
+        this.isBussinessAccount = true;
+    }
+
+    myProfile() {
+        return `my name ${this.username}, is ${this.isBussinessAccount ? 'Business' : 'Personal'}`;
+    }
+}
+
+const wa1 = new WhatsApp('080110002222');
+console.log(wa1.myProfile());
+console.log(wa1.sendMessage('halo', '089000999888'));
