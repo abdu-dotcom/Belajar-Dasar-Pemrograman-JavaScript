4c46676be797f454462ab6053985e6bd8423cd84
M. Abduh Husaini Batubara <muhammadabdu089@gmail.com>
2022-07-08 10:15:34 +0700

    without prototype

--- OOP/main.js
+++ OOP/main.js
@@ -3,15 +3,16 @@ class Mail {
     constructor() {
         this.from = 'pengirim@dicoding.com';
         this.contacts = []
-        this.yourOtherProperty = 'the values';
     }
 
-    sendMessage(msg, to) {
+    sendMessage(msg, to, from) {
         console.log(`you send: ${msg} to ${to} from ${this.from}`);
+        // from disini merujuk ke `from` paramater, bukan ke `from` dari global value yaitu pengirim@dicoding.com
         this.contacts.push(to);
     }
 }
 
 const mail1 = new Mail();
-mail1.yourOtherProperty();
-mail1.sendMessage('hello', 'penerima@dicoding.com');
+mail1.sendMessage('hello', 'penerima@dicoding.com', 'aku@gmail.com');
+console.log(mail1.from);
+console.log(mail1.contacts);
