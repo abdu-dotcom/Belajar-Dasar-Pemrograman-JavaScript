93c2aaf4f4a98e65b91d6f1b874edf3660cb1954
M. Abduh Husaini Batubara <muhammadabdu089@gmail.com>
2022-07-09 13:28:47 +0700

    Functional Programming (Imperatif and Deklaratif)

--- Functional-Programming/main.js
+++ Functional-Programming/main.js
@@ -3,10 +3,16 @@
     Dimana proses pengisian nilai array baru (newNames) 
     berdasarkan array lama (names) dilakukan secara manual.
 */
-const names = ['Harry', 'Ron', 'Jeff', 'Thomas'];
-const newNamesWithExcMark = [];
+const Imperatif_names = ['Harry', 'Ron', 'Jeff', 'Thomas'];
+const Imperatif_newNamesWithExcMark = [];
 
-for (let i = 0; i < names.length; i++) {
-    newNamesWithExcMark.push(`${names[i]}!`);
+for (let i = 0; i < Imperatif_names.length; i++) {
+    Imperatif_newNamesWithExcMark.push(`${Imperatif_names[i]}!`);
 }
-console.log(newNamesWithExcMark);
+console.log(`Output Imperatif: ${Imperatif_newNamesWithExcMark}`);
+
+// gaya penulisan deklaratif
+const Deklaratif_names = ['Harry', 'Ron', 'Jeff', 'Thomas'];
+const Deklaratif_newNamesWithExcMark = Deklaratif_names.map((name) => `${name}!`);
+
+console.log(`Output Deklaratif: ${Deklaratif_newNamesWithExcMark}`);
