98c173c1ad46474dde6333d72870babf161375e9
M. Abduh Husaini Batubara <muhammadabdu089@gmail.com>
2022-07-09 12:23:20 +0700

    Kuis

--- OOP/built-in-class.js
+++ OOP/built-in-class.js
@@ -1,12 +1,35 @@
-// parameter birthday dapat berupa miliseconds ataupun date string
-const myAge = birthday => {
-    const birtday = new Date(birthday);
-    const today = Date.now(); // today menghasil nilai miliseconds saat ini
-
-    const diff_ms = today - birtday.getTime(); // menghitung selish nilai miliseconds hari ini dan tanggal lahir 
-    const diffDate = new Date(diff_ms);
-
-    return diffDate.getFullYear() - 1970; // 1970 adalah representasi 0 dari miliseconds
-
+class Animal {
+    constructor(name, age, isMammal) {
+        this.name = name;
+        this.age = age;
+        this.isMammal = isMammal;
+    }
 }
-console.log(myAge('2001-05-12'));
+
+// Task 2
+class Rabbit extends Animal {
+    constructor(name, age) {
+        super(name, age, true);
+        this.name = name;
+    }
+    eat() {
+        return `${this.name} sedang makan!`;
+    }
+}
+
+class Eagle extends Animal {
+    constructor(name, age) {
+        super(name, age, false);
+        this.name = name;
+    }
+    fly() {
+        return `${this.name} sedang terbang!`;
+    }
+}
+const myRabbit = new Rabbit('Labi', 2);
+const myEagle = new Eagle("Elo", 4);
+
+console.log(myRabbit.eat());
+console.log(myRabbit.isMammal);
+console.log(myEagle.fly());
+console.log(myEagle.isMammal);
