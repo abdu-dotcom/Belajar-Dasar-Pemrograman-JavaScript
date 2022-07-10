0e0706c3be1eb3933d3ca6d6602a7a6c51788396
M. Abduh Husaini Batubara <muhammadabdu089@gmail.com>
2022-07-10 14:06:30 +0700

    Throwing Errors

--- Penanganan-Error/main.js
+++ Penanganan-Error/main.js
@@ -1,11 +1,22 @@
+let json = '{ "name": "Yoda", "age": 20 }';
+
 try {
-    // kode
-    console.log("Awal blok try");
+    let user = JSON.parse(json);
+
+    if (!user.name) {
+        throw new SyntaxError("'name' is required.");
+    }
+
     errorCode;
-    console.log("Akhir blok try");
+
+    console.log(user.name); // Yoda
+    console.log(user.age); // 20
 } catch (error) {
-    // error handling
-    console.log("Terjadi error! - " + error.message);
-} finally {
-    console.log("Akan tetap dieksekusi");
+    if (error instanceof SyntaxError) {
+        console.log(`JSON: ${error.message}`);
+    } else if (error instanceof ReferenceError) {
+        console.log(error.message);
+    } else {
+        console.log(error.stack);
+    }
 }
