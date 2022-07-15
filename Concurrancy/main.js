8fadd7073904b02800b3f642ac719353221ad3a0
M. Abduh Husaini Batubara <muhammadabdu089@gmail.com>
2022-07-15 09:40:46 +0700

    Concurrency : Promise All

--- Concurrancy/main.js
+++ Concurrancy/main.js
@@ -12,12 +12,31 @@ function makeEspresso() {
         })
         .then((value) => {
             console.log(value);
+            const promises = [boilWater(), grindCoffeeBeans()];
+
+            return Promise.all(promises);
         })
         .catch((rejectedReason) => {
             console.log(rejectedReason);
         })
 }
+const boilWater = () => {
+    return new Promise((resolve, reject) => {
+        console.log("Memanaskan air...");
+        setTimeout(() => {
+            resolve("Air panas sudah siap!");
+        }, 2000);
+    })
+}
 
+const grindCoffeeBeans = () => {
+    return new Promise((resolve, reject) => {
+        console.log("Menggiling biji kopi...");
+        setTimeout(() => {
+            resolve("Kopi sudah siap!");
+        }, 1000);
+    })
+}
 const state = {
         stock: {
             coffeeBeans: 250,
