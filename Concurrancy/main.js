bd96ad6c01775ef339ae8827ed887a0e66ca4bee
M. Abduh Husaini Batubara <muhammadabdu089@gmail.com>
2022-07-15 10:30:14 +0700

    Concurrancy : DONE

--- Concurrancy/main.js
+++ Concurrancy/main.js
@@ -1,25 +1,31 @@
-const getCoffee = () => {
-    return new Promise((resolve, reject) => {
-        const seeds = 100;
-        setTimeout(() => {
-            if (seeds >= 101) {
-                resolve("Kopi didapatkan!");
-            } else {
-                reject("Biji kopi habis!");
-            }
-        }, 1000);
-    })
+class NetworkError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = 'NetworkError';
+    }
 }
 
-// Promise 
-async function makeCoffee() {
+// TODO: 1
+const fetchingUserFromInternet = (isOffline) => {
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (!isOffline) {
+                resolve({ name: 'John', age: 18 })
+            } else {
+                reject(new NetworkError('Gagal mendapatkan data dari internet'));
+            }
+        }, 500);
+    })
+};
+
+
+// TODO: 2
+const gettingUserName = async() => {
     try {
-        const coffee = await getCoffee();
-        console.log(coffee);
+        const user = await fetchingUserFromInternet(false);
+        return user.name;
     } catch (rejectedReason) {
         console.log(rejectedReason);
     }
-
-}
-
-makeCoffee();
+};
