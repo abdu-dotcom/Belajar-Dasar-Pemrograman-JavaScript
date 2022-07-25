a723da37a31bfd8a6409b82c294e731a20194146
M. Abduh Husaini Batubara <muhammadabdu089@gmail.com>
2022-07-25 11:28:16 +0700

    ...

---
+++ JavascriptTesting/latihanTesting/gradeCalculations.js
@@ -0,0 +1,31 @@
+/* 
+    Menghitung rata-rata nilai siswa dari sebuah inputan berupa
+    daftar angka dalam bentuk array.
+*/
+const averageExams = (valuesExam) => {
+    const numberValidation = valuesExam.every(exam => typeof exam === 'number');
+    if (!numberValidation) throw Error('please input number');
+
+    const sumValues = valuesExam.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    return sumValues / valuesExam.length;
+};
+
+/**
+ * Melakukan kalkulasi apakah seorang siswa lulus ujian atau tidak 
+ * berdasarkan nilai rata-rata yang didapatkan (bergantung pada 
+ * fungsi averageExams)
+ */
+const isStudentPassExam = (valuesExam, name) => {
+    const minValues = 75;
+    const average = averageExams(valuesExam);
+
+    if (average > minValues) {
+        console.log(`${name} pass the exams`);
+        return true;
+    } else {
+        console.log(`${name} fail the exams`);
+        return false;
+    }
+};
+
+module.exports = { averageExams, isStudentPassExam };
